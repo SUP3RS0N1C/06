72093c119e776c7330bf620a2dce06e4f14981a1
SUP3RS0N1C <sputnik1957@sookmyung.ac.kr>
2019-10-15 15:20:10 +0900

    04

--- main.c
+++ main.c
@@ -2,39 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//정수제곱을 계산하는 함수예제
 
-int sumTwo( int a, int b)
+int square( int n);
+
+int main()
 {
-	int output;
-	output = a + b;
-	return output;
-}
+	int result;
+	result = square(5);
+	printf("%d", result);
+ } 
 
 int square(int n)
 {
-	return (n*n);
-}
-
-int get_max(int a, int b)
-{
-	if (a>b)
-		return a;
-	else
-		return b;
-}
-
-int main(int argc, char *argv[]) 
-{		
-	
-	int x, y;
-	x = 2;
-	y = 5;
-
-	int sum = sumTwo(x,y);
-	int result_square = square(x);
-	int max = get_max(x,y);
-
-	printf("the result of sumTwo is %d, square is %d, get_max is %d", sum, result_square, max);
-		
-	return 0;
+	return n*n;
 }
